3b476a2e9dc3204b36586bf51a239dd41af9c329
fabiancallejas <pfabiancallejas@gmail.com>
2022-06-06 19:16:25 -0300

    static

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -9,3 +9,7 @@ def curso (self):
     curso.save()
     documento = f"Curso: {curso.nombre} - Camada {curso.camada}"
     return HttpResponse(documento)
+
+def profesores (self):
+    documento = f"Pagina de Profesores"
+    return HttpResponse(documento)
